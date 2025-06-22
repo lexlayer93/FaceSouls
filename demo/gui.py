20e491be75da3c2fc31c92cbce1b03ac0473741c
lexlayer93 <alejandrotsr@proton.me>
2025-06-22 21:23:35 +0200

    gui update

--- demo/gui.py
+++ demo/gui.py
@@ -6,25 +6,35 @@ from facesouls.character import CharacterCreator
 from facesouls.tools import facemesh_plot
 
 
-class CCMainTk (tk.Tk):
-    def __init__ (self, *args, char_creator, **kwargs):
+class CharacterCreatorTk (tk.Tk):
+    def __init__ (self, char_creator, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        cc = char_creator
-        ccframe = CCFrameTk(self, char_creator=cc)
-        self._cc = cc
-        self._ccframe = ccframe
-        ccframe.pack(fill=tk.BOTH, expand=True)
-
         self.protocol("WM_DELETE_WINDOW", self.quit)
         self.title("Character Creator Demo")
-        menubar = tk.Menu(self)
 
-        file_menu = tk.Menu(menubar, tearoff=0)
+        cc = char_creator
+        self._cc = cc
+
+        editor = CCEditorFrame(self)
+        editor.pack(fill=tk.BOTH, expand=True)
+        self._editor = editor
+
+        menubar = CCMenu(self)
+        self.config(menu=menubar)
+        self._menubar = menubar
+
+
+class CCMenu (tk.Menu):
+    def __init__ (self, parent, *args, **kwargs):
+        super().__init__(parent, *args, **kwargs)
+        editor = parent._editor
+
+        file_menu = tk.Menu(self, tearoff=0)
         _load_menu = tk.Menu(file_menu, tearoff=0)
-        _load_menu.add_command(label="Sliders", command=self.ask_load_values)
+        _load_menu.add_command(label="Sliders values", command=self.ask_load_values)
         _load_menu.add_command(label="FaceGen FG", command=self.ask_load_data)
         _save_menu = tk.Menu(file_menu, tearoff=0)
-        _save_menu.add_command(label="Sliders", command=self.ask_save_values)
+        _save_menu.add_command(label="Sliders values", command=self.ask_save_values)
         _save_menu.add_command(label="FaceGen FG", command=self.ask_save_data)
         _export_menu = tk.Menu(file_menu, tearoff=0)
         _export_menu.add_command(label="Wavefront OBJ", command=self.ask_export_obj)
@@ -32,12 +42,15 @@ class CCMainTk (tk.Tk):
         file_menu.add_cascade(label="Save...", menu=_save_menu)
         file_menu.add_cascade(label="Export...", menu=_export_menu)
 
-        options_menu = tk.Menu(menubar, tearoff=0)
-        options_menu.add_checkbutton(label="Slider interlock", variable=ccframe._interlock)
+        options_menu = tk.Menu(self, tearoff=0)
+        options_menu.add_checkbutton(label="Slider interlock",
+                                     variable=editor._interlock,
+                                     command=editor.interlock_callback,
+                                     )
 
-        menubar.add_cascade(label="File", menu=file_menu)
-        menubar.add_cascade(label="Options", menu=options_menu)
-        self.config(menu=menubar)
+        self.add_cascade(label="File", menu=file_menu)
+        self.add_cascade(label="Options", menu=options_menu)
+
 
     def ask_load_values (self):
         path = tk.filedialog.askopenfilename(
@@ -45,8 +58,8 @@ class CCMainTk (tk.Tk):
             filetypes = [("Sliders values", "*.csv")],
             defaultextension=".csv"
             )
-        self._cc.load_values(path)
-        self._ccframe._interlock.set(not self._cc.all_at_once)
+        self.master._cc.load_values(path)
+        self.master._editor.update_all()
 
     def ask_save_values (self):
         path = tk.filedialog.asksaveasfilename(
@@ -54,7 +67,7 @@ class CCMainTk (tk.Tk):
             filetypes = [("Sliders values", "*.csv")],
             defaultextension=".csv"
             )
-        self._cc.save_values(path)
+        self.master._cc.save_values(path)
 
     def ask_load_data (self):
         path = tk.filedialog.askopenfilename(
@@ -62,8 +75,8 @@ class CCMainTk (tk.Tk):
             filetypes = [("FaceGen FG", "*.fg")],
             defaultextension=".fg"
             )
-        self._cc.load_data(path)
-        self._ccframe._interlock.set(not self._cc.all_at_once)
+        self.master._cc.load_data(path)
+        self.master._editor.update_all()
 
     def ask_save_data (self):
         path = tk.filedialog.askopenfilename(
@@ -71,7 +84,7 @@ class CCMainTk (tk.Tk):
             filetypes = [("FaceGen FG", "*.fg")],
             defaultextension=".fg"
             )
-        self._cc.save_data(path)
+        self.master._cc.save_data(path)
 
     def ask_export_obj (self):
         path = tk.filedialog.askopenfilename(
@@ -79,19 +92,18 @@ class CCMainTk (tk.Tk):
             filetypes = [("Wavefront OBJ", "*.obj")],
             defaultextension=".obj"
             )
-        self._cc.models[0].export_obj(path)
+        self.master._cc.models[0].export_obj(path)
 
 
-class CCFrameTk (tk.Frame):
-    def __init__ (self, parent, *args, char_creator, **kwargs):
+class CCEditorFrame (tk.Frame):
+    def __init__ (self, parent, *args, **kwargs):
         super().__init__(parent, *args, **kwargs)
-        cc = char_creator
+        cc = parent._cc
         self._cc = cc
         self._tkvars = dict()
         self._canvas = None
         self._polyc = None
         self._interlock = tk.BooleanVar(value=not cc.all_at_once)
-        self._interlock.trace_add("write", self.interlock_callback)
         self._debug_text = list()
 
         self.columnconfigure(0, weight=1)
@@ -135,7 +147,7 @@ class CCFrameTk (tk.Frame):
         self._polyc = facemesh_plot((verts, triangles), ax, persp="persp")
         self._ignore = False
 
-        dbframe1 = tk.Frame(self, relief="sunken", borderwidth=2)
+        dbframe1 = tk.Frame(self)
         dbframe1.grid(row=1, column=0, sticky="nsew")
         scrollbar1 = tk.Scrollbar(dbframe1)
         scrollbar1.pack(side=tk.LEFT, fill=tk.Y)
@@ -144,7 +156,7 @@ class CCFrameTk (tk.Frame):
         text1.config(yscrollcommand=scrollbar1.set)
         scrollbar1.config(command=text1.yview)
 
-        dbframe2 = tk.Frame(self, relief="sunken", borderwidth=2)
+        dbframe2 = tk.Frame(self)
         dbframe2.grid(row=1, column=1, sticky="nsew")
         scrollbar2 = tk.Scrollbar(dbframe2)
         scrollbar2.pack(side=tk.RIGHT, fill=tk.Y)
@@ -160,34 +172,39 @@ class CCFrameTk (tk.Frame):
             return
         key = int(key)
         value = self._tkvars[key].get()
-        self._cc.set_slider(key, value)
+        cc = self.master._cc
+        cc.set_slider(key, value)
         self.update_all()
 
-    def interlock_callback (self, *args, **kwargs):
-        flag = not self._interlock.get()
-        self._cc.toggle_sequence(flag)
+    def interlock_callback (self, flag=None):
+        cc = self.master._cc
+        cc.toggle_sequence(flag)
         self.update_all()
 
     def update_all (self):
         self.update_canvas()
         self.update_debug()
         self.update_tkvars()
+        cc = self.master._cc
+        self._interlock.set(not cc.all_at_once)
 
     def update_canvas (self):
-        verts = self._cc.models[0].vertices
-        triangles = self._cc.models[0].triangles_only
+        cc = self.master._cc
+        verts = cc.models[0].vertices
+        triangles = cc.models[0].triangles_only
         self._polyc.set_verts(verts[triangles])
         self._canvas.draw()
 
     def update_tkvars (self):
         self._ignore = True
+        cc = self.master._cc
         for key, int_var in self._tkvars.items():
-            value = self._cc.sliders[key].int_value
+            value = cc.sliders[key].int_value
             int_var.set(value)
         self._ignore = False
 
     def update_debug (self, fmt=".3f"):
-        cc = self._cc
+        cc = self.master._cc
         text = "FEATURES:"
         for k in (10, 20, 30):
             svalue = cc.values[k]
@@ -233,4 +250,4 @@ if __name__ == "__main__":
     face = (tri, egm)
     cc = CharacterCreator(ctl, csv, [face])
 
-    CCMainTk(char_creator=cc).mainloop()
+    CharacterCreatorTk(cc).mainloop()
